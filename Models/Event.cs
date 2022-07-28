592903469426ddf788f0b03afda021299a140908
Anthony Bonasso <bonassoa@gmail.com>
2022-07-28 15:05:32 -0400

    Added Event Images String to ADO and MainDB

--- Models/Event.cs
+++ Models/Event.cs
@@ -28,6 +28,7 @@ namespace TicketManagementProject.Models
         public string Address { get; set; }
         public string City { get; set; }
         public Nullable<int> VenueCapacity { get; set; }
+        public string EventImg { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Purchase> Purchases { get; set; }
