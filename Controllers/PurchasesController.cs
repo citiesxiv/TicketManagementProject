99c94108a687311a0f74ae603b1ec444dad8f263
Lucky Temitope Osunbiyi <osunbiyi.temitope1@gmail.com>
2022-08-05 11:22:55 -0400

    Added Purchase list to profile

---
+++ Controllers/PurchasesController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TicketManagementProject.Models;
+
+namespace TicketManagementProject.Controllers
+{
+    public class PurchasesController : Controller
+    {
+        // GET: Purchases
+        // Get User's Purchased Tickets
+        public ActionResult _PurchasedTickets()
+        {
+
+            var userId = Convert.ToInt32(Session["id"].ToString());
+
+            using (MainDBEntities dbEntities = new MainDBEntities())
+            {
+                List<Event> events = dbEntities.Events.ToList();
+                List<Purchase> purchases = dbEntities.Purchases.ToList();
+
+                try
+                {
+                    var purchaseRecord = from p in purchases
+                                   join e in events on p.EventId equals e.Id into table1
+                                   from e in table1.ToList()
+                                   where p.UserId == userId
+                                   select new PurchaseViewModel
+                                   {
+                                       events = e,
+                                       purchase = p
+                                   };
+
+                    return PartialView(purchaseRecord);
+                }
+                catch
+                {
+                    return View("Index", "Events");
+                }
+
+            }
+        }
+        }
+}
---
+++ Models/PurchaseViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TicketManagementProject.Models
+{
+    public class PurchaseViewModel
+    {
+        public Event events { get; set; }
+        public Purchase purchase { get; set; }
+    }
+}
